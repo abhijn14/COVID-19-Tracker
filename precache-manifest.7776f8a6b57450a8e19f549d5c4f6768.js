c5ce50b33ba08cfffce1ad4979c4e686eec78fe9
abhijn14 <abhijn14@gmail.com>
2020-04-27 22:28:39 +0530

    Updates

---
+++ precache-manifest.7776f8a6b57450a8e19f549d5c4f6768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5defbf1bcea82b69b128a5e4dc99ecdf",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "9ae683fc62198e1fe6af",
+    "url": "/COVID-19-Tracker/static/css/main.45285808.chunk.css"
+  },
+  {
+    "revision": "8c27fdbef60a7e28bec8",
+    "url": "/COVID-19-Tracker/static/js/2.799ff767.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.799ff767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae683fc62198e1fe6af",
+    "url": "/COVID-19-Tracker/static/js/main.7fe3b7f4.chunk.js"
+  },
+  {
+    "revision": "f04c11d52cba8a6c46a5",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.08361400.js"
+  },
+  {
+    "revision": "f694e3d54eb9412ed35d4fbca7afc1e6",
+    "url": "/COVID-19-Tracker/static/media/logo.f694e3d5.png"
+  }
+]);
