f91219d641b9bacf359a9e11bdda477113491e00
abhijn14 <abhijn14@gmail.com>
2020-04-27 21:31:45 +0530

    Fully functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//you can include multiple components like these by making index.js inside your components folder
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css';
+import { fetchData } from './api'; //If you do not mention filename, by default index.js file is imported
+
+import coronaImage from './images/logo.png'
+
+class App extends React.Component {
+
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        //fetch the data
+
+        this.setState({ data: fetchedData, country: country });
+        //set the state
+    }
+
+    render() {
+        const { data, country } = this.state;
+        console.log(data);
+
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19" />
+               <Cards data={data} />
+               <CountryPicker handleCountryChange={this.handleCountryChange} />
+               <Chart data={data} country={country}/>
+            </div>
+        )
+    }
 }
 
 export default App;
